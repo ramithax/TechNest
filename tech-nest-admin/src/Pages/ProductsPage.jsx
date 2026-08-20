3aa02b6df8ab1d4870fac47e82792491cb160804
ramitha <ramithamanilka2003@gmail.com>
2026-08-20 15:34:55 +0530

    feat: initialize admin project with axios configuration and product listing page

--- tech-nest-admin/src/Pages/ProductsPage.jsx
+++ tech-nest-admin/src/Pages/ProductsPage.jsx
@@ -1,6 +1,34 @@
-export function ProductPage() {
+import { useEffect, useState } from "react";
+import api from "../lib/axios";
+
+function ProductPage() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const getProducts = async () => {
+            try {
+                const response = await api.get("/product");
+                setProducts(response.data);
+            } catch (error) {
+                console.error("Failed to fetch products:", error);
+            }
+        };
+
+        getProducts();
+    }, []);
 
     return (
-        <p className="text-white text-2xl font-semibold text-center m-10">Products Page</p>
-    )
+        <div>
+            <h1>Products</h1>
+
+            {products.map((product) => (
+                <div key={product.id}>
+                    <h2>{product.name}</h2>
+                    <p>{product.price}</p>
+                </div>
+            ))}
+        </div>
+    );
 }
+
+export { ProductPage }
